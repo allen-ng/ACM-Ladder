b826741741dd74b7ebd709a786d0d737159507a9
Allen Ng <allen_ng@foxmail.com>
2017-01-26 11:44:05 +0800

    Fix user sign_up/update feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :check_level_up
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     if exception.action == :new && exception.subject.class == Submission
@@ -13,6 +14,21 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     session[:level] = current_user.level
     super
+  end
+
+  protected
+
+  # After rails 4, strong parameters are moved to controller
+  # This is responsible for permitting additional parameters
+  # Also, this function is added on top of this file in before_action
+  # As is stated in https://github.com/plataformatec/devise section strong parameters
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :handle, :school, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :handle, :school, :current_password, :password, :password_confirmation)
+    end
   end
 
   private
--- spec/features/guests_spec.rb
+++ spec/features/guests_spec.rb
@@ -4,8 +4,8 @@ feature 'Guest features' do
   fixtures :problems
   fixtures :settings
 
-  scenario 'Guest can sign up' do
-    visit 'http://localhost:3000/u/sign_up'
+  scenario 'Guest can sign up then edit information' do
+    visit 'u/sign_up'
     fill_in 'Email', with: 'yoha_test_test_test@acm.nankai.edu.com'
     fill_in 'Handle', with: 'youdonknowthis'
     fill_in 'user_password', with: '1234567'
@@ -13,6 +13,17 @@ feature 'Guest features' do
     click_button 'Sign up'
 
     expect(page).to have_content('Welcome')
+
+    visit 'u/edit'
+    fill_in 'Current password', with: '1234567'
+    fill_in 'user_password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    fill_in 'School', with: 'NKU'
+    click_button 'Update'
+
+    expect(page).to have_content('You updated your account successfully.')
+
+
   end
 end
 
